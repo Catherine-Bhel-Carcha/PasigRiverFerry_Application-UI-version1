2c73a9e0827a1c7e32b57dad861c73987b582911
Catherine-Bhel-Carcha <2015081521@ust-ics.mygbiz.com>
2018-11-14 00:09:06 +0800

    Initial commit

---
+++ app/src/main/java/iics/carcha/catherinebhel/pasigriverferry_application/Screen6.java
@@ -0,0 +1,13 @@
+package iics.carcha.catherinebhel.pasigriverferry_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen6);
+    }
+}
